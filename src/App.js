3c720df7debfe2be3ae51da78030ad3a40ac2897
0x53544152 <dmanifacio@csus.edu>
2025-04-22 20:59:10 -0700

    Added HomePage.jsx and adjusted some files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavigationBar from './components/NavigationBar.jsx';
 import Web3Provider from './components/Web3Context.js';
+import HomePage from "./pages/HomePage.jsx";
 import TestPage from './pages/TestPage.jsx';
 import ArtistDashboardPage from './pages/ArtistDashboardPage.jsx';
 import BuyerDashboardPage from './pages/BuyerDashboardPage.jsx';
@@ -12,9 +13,10 @@ function App() {
       <Router>
         <NavigationBar />
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/test" element={<TestPage />} /> 
-	  <Route path="/artistdashboard" element={<ArtistDashboardPage />} />
-    	  <Route path="/buyerdashboard" element={<BuyerDashboardPage />} />
+	        <Route path="/artistdashboard" element={<ArtistDashboardPage />} />
+          <Route path="/buyerdashboard" element={<BuyerDashboardPage />} />
         </Routes>
       </Router>
     </Web3Provider>
